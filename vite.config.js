7ac9c23830e0e2fb010b8c21b45d248063f08296
Muhammad Waqas Bashir <m.waqas987321@gmail.com>
2025-07-24 17:11:16 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/Blog-Post-App/',
+  base: '/Blog-Post-App/',
   plugins: [react()],
-})
+});
